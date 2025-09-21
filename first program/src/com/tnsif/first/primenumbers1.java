3355846e231c00e330e7cdd4a2951dee5e2f389a
ADMIN <ADMIN@DESKTOP-GD3O1OA>
2025-09-21 14:44:04 +0530

    Merge branch 'master' of https://github.com/Shireesha-Basaipally/java.git

---
+++ first program/src/com/tnsif/first/primenumbers1.java
@@ -0,0 +1,22 @@
+package com.tnsif.first;
+
+public class primenumbers1 {
+
+	public static void main(String[] args) {
+		System.out.println("Prime numbers from 1 to 100:");
+
+        for (int num = 2; num <= 100; num++) {
+            int i;
+            for (i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    break;
+                }
+            }
+            if (i == num) { 
+                System.out.print(num + " ");
+            }
+        }
+
+	}
+
+}

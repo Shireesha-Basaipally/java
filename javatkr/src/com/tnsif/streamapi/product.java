d5868b708db72c763035e86f8ed4bc3f55d8f28c
ADMIN <ADMIN@DESKTOP-GD3O1OA>
2025-10-01 11:49:12 +0530

    commit

---
+++ javatkr/src/com/tnsif/streamapi/product.java
@@ -0,0 +1,17 @@
+package com.tnsif.streamapi;
+
+public class product {
+	int id;
+	String name;
+	float price;
+	
+	
+	public product(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
+
+
